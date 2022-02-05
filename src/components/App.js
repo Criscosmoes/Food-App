ed6af8c889ea95e24d17c91d1a3e3d6cd6773c24
Benardo Peralta <chrisperalta1998@gmail.com>
2022-02-04 23:00:57 -0800

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>This is a test</div>;
+  return (
+    <div>
+      <a href="https://www.shopgonegirlz.com/" target="_blank">
+        Click me if cynthia stinks...
+      </a>
+      <br></br>
+      <input type="text" placeholder="Search place to eat..." />
+    </div>
+  );
 };
 
 export default App;
