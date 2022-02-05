05e6a3710b0d2aa52f5b77e0a78d6608796a399b
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-02-04 23:56:27 -0800

    added dog pic

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 const App = () => {
+  const [url, setUrl] = useState("");
+
   const fetchPlaces = async () => {
     let response = await fetch("https://dog.ceo/api/breeds/image/random");
     let places = await response.json();
-    console.log(places);
+
+    setUrl(places.message);
   };
 
   return (
-    <div class="searchbar">
+    <div className="searchbar">
       <h2>Where to eat?</h2>
 
       <input type="text" placeholder="Search restaurant..." />
       <button onClick={fetchPlaces}>Click me</button>
+      <img src={url} />
     </div>
   );
 };
