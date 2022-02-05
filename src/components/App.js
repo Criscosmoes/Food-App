30363ae1f5bfa7c7810de049d741607bd4e52a0f
Benardo Peralta <chrisperalta1998@gmail.com>
2022-02-04 23:49:26 -0800

    fetch response

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import "../App.css";
 
 const App = () => {
-  let response = fetch(url);
+  const fetchPlaces = async () => {
+    let response = await fetch("https://dog.ceo/api/breeds/image/random");
+    let places = await response.json();
+    console.log(places);
+  };
   return (
     <div>
+      <button onClick={fetchPlaces}>Click Me</button>
       <a href="https://www.shopgonegirlz.com/" target="_blank">
         Click me if maclo stinks...
       </a>
